81a648d86984f9cc7e2ee9706b1ac8551cdda3b2
Vengatesh <venkuvenky2@gmail.com>
2024-06-17 18:51:48 +0530

    Initial Commit

---
+++ src/test/java/com/med/medicineAppt/MedicineApptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.medicineAppt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineApptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
